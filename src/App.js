2738f7bdbcc5e21be0f2cead08977327cae5e447
algtan <agtan@ymail.com>
2021-02-24 16:32:17 -0800

    completed first 3 user stories

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: ["", "", "", "", "", "", "", "", ""],
+      turn: "X"
+    }
+  }
+
+  userClick = (index, turn) => {
+    const { squares } = this.state
+    squares[index] = turn
+    if (turn === "X") {
+      this.setState({ squares: squares, turn: "O"})
+    } else {
+      this.setState({ squares: squares, turn: "X"})
     }
   }
 
@@ -14,7 +25,19 @@ class App extends Component{
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        <div className="gameboard">
+        { this.state.squares.map((value, index) => {
+            return (
+              <Square 
+                value={ value }
+                key={ index }
+                index={ index }
+                userClick={ this.userClick }
+                turn = { this.state.turn }
+              />
+            )
+        })}
+        </div>
       </>
     )
   }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 class Square extends Component{
+
+  handleClick = () => {
+    // alert('clicked')
+    this.props.userClick(this.props.index, this.props.turn)
+  }
+
   render(){
     return(
       <>
-        <div className="square"></div>
+        <div className="square" onClick={ this.handleClick }>{ this.props.value }</div>
       </>
     )
   }
